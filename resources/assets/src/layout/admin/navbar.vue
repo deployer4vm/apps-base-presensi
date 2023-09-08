fd7cfc9564a3fababb56315ab4bf6f38710b39a1
Ahmad Dimyati <dimyati336@gmail.com>
2023-09-08 18:47:38 +0700

    fix bug nama koperasi di tm

--- resources/assets/src/layout/admin/navbar.vue
+++ resources/assets/src/layout/admin/navbar.vue
@@ -13,7 +13,7 @@
                 <img v-else :src="`${publicUrl}assets/images/koperasi_logo.png`" alt="">
             </span>
             <div class="app-brand-text d-none d-lg-block  nav-item text-big font-weight-light line-height-1 opacity-50 mr-2 ml-3">|</div>
-            <h5 class="app-brand-text d-none d-lg-block  demo ml-2 mb-0">{{ koperasi.nama }}</h5>
+            <h5 class="app-brand-text d-none d-lg-block  demo ml-2 mb-0">{{ title }}</h5>
         </b-navbar-brand>
 
         <!-- Navbar toggle -->
@@ -113,6 +113,7 @@
 export default {
     data(){
         return {
+            title: '',
             koperasi: {},
             ActiveRoleCode: ''
         };
@@ -140,6 +141,7 @@ export default {
             this.ActiveRoleCode = this.UserAuth.getAuthRole().role_code;
 
         this.koperasi = this.Web.getTenant.instance_data
+        this.title = this.koperasi ? this.koperasi.nama : this.Web.getAdminTitle()
     },
     methods: {
         changeRole(roleCode) {
