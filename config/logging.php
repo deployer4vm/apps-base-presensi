10194b18ff8f746229d375553702b51cc3ed2edc
Harry Prayudhi <kang.harry.p@gmail.com>
2024-11-15 20:37:46 +0700

    update config logging

--- config/logging.php
+++ config/logging.php
@@ -46,6 +46,7 @@ return [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
+            'permission' => 0664,
         ],
 
         'daily' => [
@@ -53,6 +54,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
             'days' => 14,
+            'permission' => 0664,
         ],
 
         'slack' => [
